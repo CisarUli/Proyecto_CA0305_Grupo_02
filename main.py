5dc93ad319ba905b05dc168194a219b559836ae0
gorillaicecream <43622455+gorillaicecream@users.noreply.github.com>
2025-07-10 14:11:54 -0600

    Update main.py
    Modifica ruta a los datos

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from BPANN import BPANN
 from LimpiezaDatos import LimpiezaDatos
 from NB import NB
 
-datos_limpios = LimpiezaDatos("Telco-Customer-Churn4.csv")
+datos_limpios = LimpiezaDatos("datos/Telco-Customer-Churn4.csv")
 datos_limpios.limpiar()
 
 bpan = BPANN(datos_limpios.datos[0], datos_limpios.datos[1])
