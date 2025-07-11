92673606dabb1f672a12380a47f64c8c68d7f601
gorillaicecream <43622455+gorillaicecream@users.noreply.github.com>
2025-07-10 18:03:45 -0600

    Update main.py
    Aparecen todos los modelos

--- main.py
+++ main.py
@@ -4,12 +4,15 @@ sys.path.append('codigo/')
 from BPANN import BPANN
 from LimpiezaDatos import LimpiezaDatos
 from NB import NB
+from XGBModel import XGBModel
 
 datos_limpios = LimpiezaDatos("datos/Telco-Customer-Churn4.csv")
 datos_limpios.limpiar()
 
 bpan = BPANN(datos_limpios.datos[0], datos_limpios.datos[1])
 nb = NB(datos_limpios.datos[0], datos_limpios.datos[1])
+xgboost = XGBModel()
 
 bpan.probar()
 nb.probar()
+xgboost.evaluar(datos_limpios.datos[0], datos_limpios.datos[1])
